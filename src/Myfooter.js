2f2b1b25910da2b27fddeaabe81838ced183e5e5
Crimsongem <jaffy038@gmail.com>
2022-07-18 10:54:16 +0700

    ADD

---
+++ src/Myfooter.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function MyFooter() {
+    return (
+      <div className='My-footer'>
+        <span><center>Developed by Methasit C. @2022</center></span> 
+      </div>
+    )
+  }
+  export default MyFooter;
---
+++ src/Myheader.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function MyHeader() {
+    return (
+      <div className='My-header'>
+        <h1><center>React Demo Site</center></h1>
+      </div>
+    );
+  }
+
+  export default MyHeader;
